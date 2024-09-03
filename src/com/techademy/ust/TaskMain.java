48fe22a4ae9c3f1368b86ac45e7126c8ee4e5bf1
Bhavithra-Jeyasubramanian <Bhavithra.Jeyasubramanian@ust.com>
2024-09-03 14:18:53 +0530

    TaskProgram_InitialCommit

---
+++ src/com/techademy/ust/TaskMain.java
@@ -0,0 +1,28 @@
+package com.techademy.ust;
+
+import java.time.LocalDate;
+
+public class TaskMain {
+	
+	public static void main(String[] args) {
+		
+		ToDoList list=new ToDoList(5);
+		
+		Task task1=new Task("Complete module Videos",LocalDate.of(2024, 9, 9));
+		Task task2=new Task("Complete Assignments",LocalDate.of(2024, 9, 20));
+		Task task3=new Task("Complete Java Course",LocalDate.of(2024, 11, 20));
+		Task task4=new Task("Complete Sample project",LocalDate.of(2024, 10, 23));
+		
+		list.addTask(task1);
+		list.addTask(task2);
+		list.addTask(task3);
+		list.addTask(task4);
+		
+		list.dispalyTask();
+		
+		list.removeTask(task3);
+		
+		list.dispalyTask();
+	}
+
+}
---
+++ src/com/techademy/ust/ToDoList.java
@@ -0,0 +1,71 @@
+package com.techademy.ust;
+
+public class ToDoList {
+	
+	Task[] tasks;
+	int taskCount=0;
+	public ToDoList(int size)
+	{
+		tasks=new Task[size];
+	}
+	
+	public void addTask(Task task)
+	{
+		if(taskCount<this.tasks.length)
+		{
+		   tasks[taskCount]=task;
+		   taskCount++; 
+		}
+		else
+		{
+			System.out.println("Task Cannot be added");
+		}
+	}
+	
+	public void removeTask(Task toRemoveTask)
+	{
+		int index=0;
+		for(int i=0;i<tasks.length;i++)
+		{
+			if(tasks[i].equals(toRemoveTask))
+			{
+				tasks[i]=null;
+				index=i;
+				break;
+			}
+			
+		}
+		
+		
+		if(index<tasks.length)
+		{
+		 for(int i=index;i<tasks.length-1;i++)
+		 {
+		 	tasks[i]=tasks[i+1];
+		 }
+		taskCount--;
+		}
+		else
+		{
+			System.out.println("Task not found");
+		}
+	}
+	
+	public void dispalyTask()
+	{
+		for(Task task:tasks)
+		{
+			if(task!=null)
+			{
+			System.out.println("Task:"+task.getTask());
+			System.out.println("DueDate:"+task.getDueDate());
+			}
+			else
+			{
+				break;
+			}
+		}
+		System.out.println();
+	}
+
+}
