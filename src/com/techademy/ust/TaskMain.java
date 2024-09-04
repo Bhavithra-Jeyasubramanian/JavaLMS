b7e68cd8da08ae0e39a48cc614517bad5deb1725
Bhavithra-Jeyasubramanian <Bhavithra.Jeyasubramanian@ust.com>
2024-09-04 12:15:03 +0530

    UpdatedTaskMain

--- src/com/techademy/ust/TaskMain.java
+++ src/com/techademy/ust/TaskMain.java
@@ -9,8 +9,8 @@ public class TaskMain {
 		ToDoList list=new ToDoList(3);
 		
 		Task task1=new Task("Complete module Videos",LocalDate.of(2024, 9, 9));
-		Task task2=new Task("Complete Assignments",LocalDate.of(2024, 9, 20));
-		Task task3=new Task("Complete Java Course",LocalDate.of(2024, 11, 20));
+		Task task3=new Task("Complete Assignments",LocalDate.of(2024, 9, 20));
+		Task task2=new Task("Complete Java Course",LocalDate.of(2024, 11, 20));
 		Task task4=new Task("Completee Sample project",LocalDate.of(2024, 10, 23));
 		
 		list.addTask(task1);
@@ -18,13 +18,19 @@ public class TaskMain {
 		list.addTask(task3);
 		list.addTask(task4);
 		
+		System.out.println("Before Prioritization");
 		list.dispalyTask();
 		
-		list.removeTask(task3);
+		System.out.println("After Prioritization");
+		list.prioritizeTasks();
 		
 		list.dispalyTask();
 		
-		list.removeTask(task3);
+		list.removeTask(task2);
+		
+		list.dispalyTask();
+		
+		list.removeTask(task2);
 	}
 
 }
